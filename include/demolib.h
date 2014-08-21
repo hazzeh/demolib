85ebe4ba3f77ddf2284952cc3a6c2bd9f78462ec
Anders Ro <anders.ronnbrant@gmail.com>
2014-08-21 02:39:07 +0200

    Update token function and add message converter

--- include/demolib.h
+++ include/demolib.h
@@ -3,8 +3,11 @@
 #ifndef DEMOLIB_H
 #define DEMOLIB_H
 
+#include <cstdlib>
+
 extern int const SEED_VALUE;
 
-int getToken(int);
+int32_t getToken(int32_t seed);
+char const* getMessage(int32_t token);
 
 #endif
--- src/demolib.cpp
+++ src/demolib.cpp
@@ -4,15 +4,35 @@
 
 #include <cstdio>
 
-int const SEED_VALUE = 1234;
+int32_t const SEED_VALUE = 32;
 
-int getToken(int token)
-{
-  if (token < 10) {
+int32_t
+getToken(int32_t seed) {
+  if (seed < 10) {
     return 10;
-  } else if (token < 20) {
+  } else if (seed < 20) {
     return 20;
   } else {
-    return token;
+    return seed;
+  }
+}
+
+char const*
+getMessage(int32_t token) {
+  switch (token) {
+    case 10:
+      return "token10";
+      break;
+    case 20:
+      return "token20";
+      break;
+    case 30:
+      return "token30";
+      break;
+    case 40:
+      return "token40";
+      break;
+    default:
+      return "undefined";
   }
 }
